68c2b532851c50990a3534d95e31b5196cdd13a9
fuchinoko <flimeblock@gmail.com>
2024-06-30 19:35:11 +0300

    Updates

---
+++ precache-manifest.cc9f04874e6bb8a36f2ac2b6dbe90fe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5cdb28d3fc01dc77a5a3808b626c89",
+    "url": "/tiktoks-of-my-wife/index.html"
+  },
+  {
+    "revision": "9143491242291df8a0b1",
+    "url": "/tiktoks-of-my-wife/static/css/main.616b9e1c.chunk.css"
+  },
+  {
+    "revision": "c98352b646af7c082338",
+    "url": "/tiktoks-of-my-wife/static/js/2.884fc344.chunk.js"
+  },
+  {
+    "revision": "9143491242291df8a0b1",
+    "url": "/tiktoks-of-my-wife/static/js/main.52bf9599.chunk.js"
+  },
+  {
+    "revision": "d342f4914db4332d4959",
+    "url": "/tiktoks-of-my-wife/static/js/runtime~main.9612bb70.js"
+  }
+]);
